24879080c9fc57abbce11137cff374f38ffe9d0a
danielcobo <danielcobo@users.noreply.github.com>
2021-10-24 13:48:46 +0200

    Remove polyfill for NodeJS - do it manually

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 const newOutput = function newOutput(format) {
